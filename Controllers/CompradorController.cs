8205db6cd847c954b68c6da22dbe5e5d95fd12b8
Ellen.Kos <143337580+KruxKossel@users.noreply.github.com>
2024-11-18 20:42:56 -0300

    att homes graficos
    home comprador e vendedor com graficos

--- Controllers/CompradorController.cs
+++ Controllers/CompradorController.cs
@@ -6,18 +6,30 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Plantech.Interfaces;
 
 namespace Plantech.Controllers
 {
     [Authorize(Roles = "Comprador, Administrador")]
-    public class CompradorController(ILogger<CompradorController> logger) : Controller
+    public class CompradorController(ILogger<CompradorController> logger, ILotesInsumosService lotesInsumosService) : Controller
     {
         private readonly ILogger<CompradorController> _logger = logger;
 
-        public IActionResult Index()
+        private readonly ILotesInsumosService _lotesInsumosService = lotesInsumosService;
+
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var lotes = await _lotesInsumosService.ListarLotes();
+            var lotesAgrupados = lotes
+                .GroupBy(l => l.Nome)
+                .Select(g => new Plantech.DTOs.LotesInsumoDTO 
+                { 
+                    Nome = g.Key, 
+                    Quantidade = g.Sum(l => l.Quantidade) 
+                }).ToList();
+            return View(lotesAgrupados);
         }
+
 
     }
 }
--- Controllers/VendedorController.cs
+++ Controllers/VendedorController.cs
@@ -6,17 +6,32 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Plantech.DTOs;
+using Plantech.Interfaces;
 
 namespace Plantech.Controllers
 {
     [Authorize(Roles = "Vendedor, Administrador")]
-    public class VendedorController(ILogger<VendedorController> logger) : Controller
+    public class VendedorController(ILogger<VendedorController> logger, IVendasService vendasService) : Controller
     {
         private readonly ILogger<VendedorController> _logger = logger;
 
-        public IActionResult Index()
+        private readonly IVendasService _vendasService = vendasService;
+
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var vendas = await _vendasService.ListarVendas();
+            var vendasAgrupadas = vendas
+                .GroupBy(v => v.Data)
+                .Select(g => new VendaDTO 
+                { 
+                    Data = g.Key, 
+                    TotalVendas = g.Sum(v => v.TotalVendas) 
+                }).ToList();
+            return View(vendasAgrupadas);
         }
+
+
+
     }
 }
